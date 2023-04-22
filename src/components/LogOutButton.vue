35cf6f5a835f78719e8e7fbb0da3b911cd4a27b0
isahua16 <isaelhuard@gmail.com>
2023-04-22 16:01:22 -0600

    adding a logout button

---
+++ src/components/LogOutButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <button @click="handle_logout">Log out</button>
+    </div>
+</template>
+
+<script>
+import cookies from 'vue-cookies'
+    export default {
+        methods: {
+            handle_logout: function () {
+                cookies.remove(`wins`);
+                cookies.remove(`losses`);
+                cookies.remove(`token`);
+                this.$router.push(`/`);
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/GamePage.vue
+++ src/views/GamePage.vue
@@ -2,17 +2,20 @@
     <div>
         <score-board></score-board>
         <game-button></game-button>
+        <log-out-button></log-out-button>
     </div>
 </template>
 
 <script>
 import GameButton from '@/components/GameButton.vue'
 import ScoreBoard from '@/components/ScoreBoard.vue'
+import LogOutButton from '@/components/LogOutButton.vue'
 import cookies from 'vue-cookies'
     export default {
         components: {
             GameButton,
-            ScoreBoard
+            ScoreBoard,
+            LogOutButton
         },
         mounted () {
             if(cookies.get(`token`) == null) {
